55f9e7ed7ef6f8a5eb95bfeaeedf3401fa1998c3
Sean Timm <seantimm3@aol.com>
2021-05-08 22:37:13 -0600

    update: still fledgling stages.

---
+++ website/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css'
+
+class Navbar extends React.Component {
+    render() {
+        return (
+        <div className="Navbar">
+            <h1>moshi moshi!</h1>
+            <button className="btn btn-primary"></button>
+        </div>
+        )
+    }
+}
+
+export default Navbar
