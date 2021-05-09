75f4594a74a37492101fbc89fe5c0e824e627bf4
Sean Timm <seantimm3@aol.com>
2021-05-09 15:06:19 -0600

    refactor: add in react-bootstrap and react-icons.

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import {FaBiohazard} from 'react-icons/fa'
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Header from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Header></Header>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FaBiohazard className="App-logo" size={150}/>
       </header>
     </div>
   );
--- website/src/components/Navbar/Navbar.tsx
+++ website/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
+import {FaBomb, FaBiohazard, FaPaperPlane} from 'react-icons/fa'
+class Header extends React.Component {
+  icon_size = 22;
 
-class Navbar extends React.Component {
-    render() {
-        return (
-        <div className="Navbar">
-            <h1>moshi moshi!</h1>
-            <button className="btn btn-primary"></button>
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div className="container-fluid">
+          <ul className="navbar-nav">
+            <li>
+              <h2 className="nav-brand">University of New Mexico Ultimate</h2>
+            </li>
+          </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="http://www.unm.edu/~stimm48/Men.html"
+              >
+                <FaBiohazard size={this.icon_size}/> Hanta Virus
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                <FaBomb size={this.icon_size}/> Bombshells
+              </a>
+            </li>
+          </ul>
         </div>
-        )
-    }
+      </nav>
+    );
+  }
 }
 
-export default Navbar
+export default Header;
