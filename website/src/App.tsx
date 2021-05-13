6af6711c3d735a642fb7b9d1f5efd8220dc9caca
Sean Timm <seantimm3@aol.com>
2021-05-12 20:43:48 -0600

    update: add spinning logos

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import {FaBiohazard} from 'react-icons/fa'
-import './App.css';
-import Header from './components/Navbar/Navbar';
+import React from "react";
+import { FaBiohazard, FaBomb } from "react-icons/fa";
+import "./App.css";
+import Header from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <header className="App-header">
-        <FaBiohazard className="App-logo" size={150}/>
+        <div className="row">
+          <div className="col">
+            <FaBiohazard className="App-logo" size={150} />
+          </div>
+          <div className="col">
+            <FaBomb className="App-logo" size={150} />
+          </div>
+        </div>
+        <p>Coming to a University of New Mexico Near You!</p>
       </header>
     </div>
   );
